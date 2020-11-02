bcf402c8d80620cedfbeaab5f519608947c11712
xiyuy98 <57184392+xiyuy98@users.noreply.github.com>
2020-11-01 18:33:26 -0600

    Add files via upload

--- code/step2_(group project)_Xiyucode_extractFDR.R
+++ code/step2_(group project)_Xiyucode_extractFDR.R
@@ -2,21 +2,41 @@
 setwd("C:/Users/Xiyu/Desktop/Xiyu's Folder/2020 Fall/Stat 992")
 
 library(tidyverse)
-source("code/step2_(group project)_Xiyucode_getData.R")
+source("code/step2_(group project)_Karlcode_getData.R")
 
 start_time = Sys.time()
 
 includeLine = function(x) {
   if(nchar(x$paperAbstract) == 0) return(F) 
-  grepl("false discovery rate", x$paperAbstract) # capitalization? FDR?
+  grepl("false discovery rate|False Discovery Rate|False discovery rate", x$paperAbstract)
 }
 
-# 1. inclusion criteria: 1) citations (popular ones); 2) abstract
+processLine = function(x) tibble(
+  id = x$id,
+  title = x$title,
+  abstract = x$paperAbstract,
+  year = x$year,
+  field = paste(x$fieldsOfStudy, collapse='; '),
+  author = paste(x$authors, collapse='; '),
+  inCitation = paste(x$inCitations, collapse='; '),
+  outCitation = paste(x$outCitations, collapse='; '),
+  journalName = x$journalName,
+  journalVolume = x$journalVolume,
+  journalPages = x$journalPages)
 
-processLine = function(x) tibble(title = x$title, 
-                                 abstract = x$paperAbstract,
-                                 year = x$year)
-outputPath = "FDR"
+# tibble(id = x$id,
+# title = x$title,
+# abstract = x$paperAbstract,
+# year = x$year,
+# field = x$fieldsOfStudy,
+# author = x$authors,
+# inCitation = x$inCitations,
+# outCitation = x$outCitations,
+# journalName = x$journalName,
+# journalVolume = x$journalVolume,
+# journalPages = x$journalPages)
+
+outputPath = "FDR_test"
 processDataFiles(includeLine, processLine, outputPath)
 
 end_time = Sys.time()
---
+++ code/step2_(group project)_Xiyucode_test.R
@@ -0,0 +1,15 @@
+# test1
+addThis = "00000"
+addThis = tibble(addThis)
+X = addThis
+addThis = as.character(list("1", "2"))
+addThis
+addThis = tibble(addThis)
+X = add_row(X, addThis)
+X
+
+# test2
+addThis = paste(list('A', 'B'), collapse='; ')
+addThis = tibble(addThis)
+
+# test3 outCitations
