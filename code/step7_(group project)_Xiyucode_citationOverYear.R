55dee2bad794515519d2ed0a1e10ade17bdfde7f
xiyuy98 <57184392+xiyuy98@users.noreply.github.com>
2020-12-07 23:44:37 -0600

    Add files via upload

--- code/step7_(group project)_Xiyucode_citationOverYear.R
+++ code/step7_(group project)_Xiyucode_citationOverYear.R
@@ -6,6 +6,7 @@ source("code/step6_(group project)_Xiyucode_clusterAssignment.R")
 
 # package
 library(ggplot2)
+library(scater)
 
 # read in data
 years <- readRDS("data/years.RDS")
@@ -16,6 +17,44 @@ FDR_years <- years %>%
   filter(year %in% c("1995":"2019")) %>% 
   select(year, paperNum_total)
 FDR_years$year = c(1995:2019)
+
+# multiplot function created by Shuqi
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
 
 ## inCitation network
 # combine FDR_out with FDR_years
